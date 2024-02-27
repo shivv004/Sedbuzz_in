ac8b2feffcf2b4d93262fdd128a9486b057c02d4
shivv004 <shiv.singh1104@gmail.com>
2024-02-27 20:19:33 +0530

    first commit-final

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,23 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { hydrate, render } from "react-dom";
 import App from './App.jsx'
 import './index.css'
 import { store, persistor } from './redux/store.jsx'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 
-const App1 = (
+ReactDOM.createRoot(document.getElementById('root')).render(
   <PersistGate persistor={persistor}>
     <Provider store={store}>
       <App />
     </Provider>
   </PersistGate>
-);
- 
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  hydrate(App1, rootElement);
-} else {
-  render(App1, rootElement);
-}
+)
